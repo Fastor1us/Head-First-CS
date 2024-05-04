5679dc80553f4dfead09f9af83ae16860a23efc0
Oleg Kiryushin <fewgwer3@yandex.ru>
2024-05-04 16:00:23 +0300

    [sln]: SwordDamageUI

---
+++ SwordDamageUI/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace SwordDamageUI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SwordDamage swordDamage = new SwordDamage(3, 2, 1.75f);
+
+            int damage = swordDamage.CalculateDamage((bool)magicCheckBox.IsChecked, (bool)flamingCheckBox.IsChecked);
+
+            damageTextBlock.Text = "Rolled " + swordDamage.Roll + " for " + damage.ToString() + "HP";
+        }
+    }
+}
---
+++ SwordDamageUI/SwordDamage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SwordDamageUI
+{
+    internal class SwordDamage
+    {
+        public int BaseDamage { get; private set; }
+        public int FlameDamage { get; private set; }
+        public int Roll { get; private set; }
+        public float MagicMultiplier { get; private set; }
+        private static Random random = new Random();
+
+        public SwordDamage(int baseDamage, int flameDamage, float magicMultiplier)
+        {
+            BaseDamage = baseDamage;
+            FlameDamage = flameDamage;
+            MagicMultiplier = magicMultiplier;
+        }
+        
+        public int CalculateDamage(bool isMagic = false, bool isFlaming = false)
+        {
+            for (byte i = 0; i < 3; i++) Roll += random.Next(1, 7);
+            float magicMultiplier = isMagic ? MagicMultiplier : 1;
+            int flameDamage = isFlaming ? FlameDamage : 1;
+            return (int)(Roll * magicMultiplier) + flameDamage + BaseDamage;
+        }
+    }
+}
