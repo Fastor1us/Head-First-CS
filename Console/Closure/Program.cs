c9e16583e01e29c2d8a37eba7504f37260b82233
Oleg Kiryushin <fewgwer3@yandex.ru>
2024-08-31 13:48:06 +0300

    [feat]: add Closure

---
+++ Console/Closure/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Closure
+{
+    internal class Program
+    {
+        static Func<int> CreateSquare()
+        {
+            int x = 2;
+            return () => ++x * x;
+        }
+
+        static void Main(string[] args)
+        {
+            Func<int> square = CreateSquare();
+
+            Console.WriteLine(square());
+            Console.WriteLine(square());
+            Console.WriteLine(square());
+        }
+    }
+}
